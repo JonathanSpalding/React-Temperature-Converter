268d85bf45b69c1d499e775a71570d46c1b63649
JonathanSpalding <Jon.Spalding@hotmail.com>
2018-04-16 22:41:13 -0600

    Create constructor, and customize button click handlers.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  celsius(degree) {
-    var x;
-    x = document.getElementById("c").value;
+
+  constructor(props) {
+    //set our initial state.
+    super(props);
+    this.state = {
+    }
+    //bind the button click handler
+    this._onFahrenheitClicked.bind(this);
+    this._onCelsiusClicked.bind(this);
+  }
+
+  _onFahrenheitClicked(event) {
+    event.preventDefault();
+    //showAnswer: !this.state.showAnswer
+    var x = document.getElementById("f").value;
+    if (isNaN(x)) {
+        document.getElementById("error").innerHTML = "Warning! '" + x + "' Is not a number!";
+        setTimeout(function(){ document.getElementById("error").innerHTML = ""; }, 5000);
+    } else {
+    x = (document.getElementById("f").value -32) * 5 / 9;
+    document.getElementById("c").value = Math.round( x * 10 ) / 10;
+    document.getElementById("error").innerHTML = "";
+    }
+  }
+  _onCelsiusClicked(event) {
+    event.preventDefault();
+    var x = document.getElementById("c").value;
     if (isNaN(x)) {
         document.getElementById("error").innerHTML = "Warning! '" + x + "' Is not a number!";
         setTimeout(function(){ document.getElementById("error").innerHTML = ""; }, 5000);
@@ -13,20 +37,6 @@ class App extends Component {
         x = document.getElementById("c").value * 9 / 5 + 32;
         document.getElementById("f").value = Math.round( x * 10 ) / 10;
         document.getElementById("error").innerHTML = "";  
-    }
-  
-  }
-
-  fahrenheit(degree) {
-    var x;
-    x = document.getElementById("f").value;
-    if (isNaN(x)) {
-        document.getElementById("error").innerHTML = "Warning! '" + x + "' Is not a number!";
-        setTimeout(function(){ document.getElementById("error").innerHTML = ""; }, 5000);
-    } else {
-    x = (document.getElementById("f").value -32) * 5 / 9;
-    document.getElementById("c").value = Math.round( x * 10 ) / 10;
-    document.getElementById("error").innerHTML = "";  
     }
   }
 
@@ -37,14 +47,14 @@ class App extends Component {
       <div className="row">
         
         <div className="col-md-6">
-          <p>Celsius</p>
-          <input id="c"  placeholder="Degree in Celsius"></input>
-          <button value=" " onClick={(fahrenheit) => this.celsius(fahrenheit)}><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span></button>
+          <h2>Celsius</h2>
+          <input id="c" defaultValue="0" placeholder="Degree in Celsius"></input>
+          <button value=" " onClick={this._onCelsiusClicked}><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>&gt;</button>
         </div>
         <div className="col-md-6">
-          <p>Fahrenheit</p>
-          <button  value=" " onClick={(celsius) => this.fahrenheit(celsius)}><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></button>
-          <input id="f" placeholder="Degree in Fahrenheit"></input>
+          <h2>Fahrenheit</h2>
+          <button  value=" " onClick={this._onFahrenheitClicked}><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>&lt;</button>
+          <input id="f" defaultValue="32" placeholder="Degree in Fahrenheit"></input>
         </div>
         <div id="error"></div>
       </div>
